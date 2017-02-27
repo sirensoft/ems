608a04e9eb8b2f861fc39d71327ab932c0c31d95
tehnn <tehnnn@gmail.com>
2017-02-27 18:21:59 +0700

    ems

--- frontend/modules/ems/views/default/index.php
+++ frontend/modules/ems/views/default/index.php
@@ -5,6 +5,7 @@ $this->params['breadcrumbs'][] = "ค้นหาผู้ป่วย";
 use yii\widgets\ActiveForm;
 use yii\helpers\Url;
 use kartik\tabs\TabsX;
+use yii\helpers\Html;
 ?>
 <div class="panel panel-info" >
 
@@ -19,7 +20,7 @@ use kartik\tabs\TabsX;
 
         <div class="input-group">
 
-            <?= $form->field($searchModel, 'search')->textInput(['placeholder' => '13หลัก/ชื่อ/นามสกุล'])->label(FALSE) ?>
+            <?= $form->field($searchModel, 'search')->textInput(['placeholder' => 'เลขบัตร/ชื่อ/นามสกุล'])->label(FALSE) ?>
             <span class="input-group-btn">
                 <button class="btn btn-default alignment" type="submit">
                     <i class="glyphicon glyphicon-search"></i> ค้นหา
@@ -41,11 +42,49 @@ echo TabsX::widget([
             ]),
         ],
         [
-            'label' => 'รายชื่อขึ้นทะเบียน',
-            'content' => $this->render('list', [
-                'searchModel' => $searchModel,
-                'dataProvider' => $dataProvider,
-            ]),
+            'label' => 'รายชื่อทะเบียน',
+            'content' => '',
         ]
     ]
 ]);
+?>
+<div id="map" style="height: 450px"></div>
+
+<?php
+
+$this->registerJsFile('//maps.googleapis.com/maps/api/js?key=AIzaSyDtrQxIgZGCXrChaNgCc0yCFCAyTFEmHU8', [
+    'async' => TRUE,
+    'defer' => TRUE,
+    'position' => \yii\web\View::POS_BEGIN
+    ]);
+?>
+
+<?php
+$js =<<<JS
+  
+
+       var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 16, lng: 100},
+          zoom: 8
+        });
+        var myLatLng = {lat: 16, lng: 100};
+        
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!',
+          draggable: true,
+        });
+   
+
+      
+      
+        
+          
+        
+      
+JS;
+$this->registerJs($js);
+
+?>
+
